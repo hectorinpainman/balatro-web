d4516d509a4d833bcfbd6197bf3ac505805c7ca2
hectorinpainman <noscopeconpene@gmail.com>
2026-09-09 19:43:50 +0100

    Fix love.js root path and service worker filter

--- sw.js
+++ sw.js
@@ -3,5 +3,6 @@ self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./', './index.html', './manifest.json', './game.js'])));
 });
 self.addEventListener('fetch', e => {
+  if (!e.request.url.startsWith('http')) return;
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
