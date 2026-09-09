6783a7097167eefb13448fe3a6ba4acce7c487ce
hectorinpainman <noscopeconpene@gmail.com>
2026-09-09 19:24:43 +0100

    Balatro PWA Deployment (Clean Split)

---
+++ patch.js
@@ -0,0 +1,14 @@
+﻿const originalFetch = window.fetch;
+window.fetch = async function(url, ...args) {
+  if (typeof url === 'string' && url.includes('game.data')) {
+    const [p1, p2] = await Promise.all([
+      originalFetch('game.data.part1').then(r => r.arrayBuffer()),
+      originalFetch('game.data.part2').then(r => r.arrayBuffer())
+    ]);
+    const combined = new Uint8Array(p1.byteLength + p2.byteLength);
+    combined.set(new Uint8Array(p1), 0);
+    combined.set(new Uint8Array(p2), p1.byteLength);
+    return new Response(combined);
+  }
+  return originalFetch(url, ...args);
+};
