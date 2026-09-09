24a867b05e9009c6e8b79c19faa8e68832bcc881
hectorinpainman <noscopeconpene@gmail.com>
2026-09-09 19:33:40 +0100

    Patch XMLHttpRequest for split game.data loading

--- patch.js
+++ patch.js
@@ -1,14 +1,47 @@
-﻿const originalFetch = window.fetch;
-window.fetch = async function(url, ...args) {
-  if (typeof url === 'string' && url.includes('game.data')) {
+﻿(function() {
+  let combinedBuffer = null;
+
+  async function getCombinedData() {
+    if (combinedBuffer) return combinedBuffer;
     const [p1, p2] = await Promise.all([
-      originalFetch('game.data.part1').then(r => r.arrayBuffer()),
-      originalFetch('game.data.part2').then(r => r.arrayBuffer())
+      fetch('game.data.part1').then(r => r.arrayBuffer()),
+      fetch('game.data.part2').then(r => r.arrayBuffer())
     ]);
-    const combined = new Uint8Array(p1.byteLength + p2.byteLength);
-    combined.set(new Uint8Array(p1), 0);
-    combined.set(new Uint8Array(p2), p1.byteLength);
-    return new Response(combined);
+    combinedBuffer = new Uint8Array(p1.byteLength + p2.byteLength);
+    combinedBuffer.set(new Uint8Array(p1), 0);
+    combinedBuffer.set(new Uint8Array(p2), p1.byteLength);
+    return combinedBuffer;
   }
-  return originalFetch(url, ...args);
-};
+
+  const OriginalXHR = window.XMLHttpRequest;
+  function PatchedXHR() {
+    const xhr = new OriginalXHR();
+    const origOpen = xhr.open;
+    const origSend = xhr.send;
+    let isGameData = false;
+
+    xhr.open = function(method, url) {
+      if (typeof url === 'string' && url.includes('game.data')) {
+        isGameData = true;
+      }
+      return origOpen.apply(this, arguments);
+    };
+
+    xhr.send = function() {
+      if (isGameData) {
+        getCombinedData().then(buffer => {
+          Object.defineProperty(xhr, 'response', { value: buffer.buffer, writable: false });
+          Object.defineProperty(xhr, 'status', { value: 200, writable: false });
+          Object.defineProperty(xhr, 'readyState', { value: 4, writable: false });
+          if (typeof xhr.onload === 'function') xhr.onload();
+          if (typeof xhr.onreadystatechange === 'function') xhr.onreadystatechange();
+        }).catch(err => console.error("Error loading split game.data:", err));
+        return;
+      }
+      return origSend.apply(this, arguments);
+    };
+
+    return xhr;
+  }
+  window.XMLHttpRequest = PatchedXHR;
+})();
